9fd8480d0308d4a826fd7ba91fe8adb459e00a9a
王亮 <1392830517@qq.com>
2021-05-14 16:13:24 +0800

    update: 优化打包

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 module.exports = {
   pages: {
     index: {
@@ -21,10 +22,26 @@ module.exports = {
     output: {
       libraryExport: 'default'
     },
-    externals: {
+    externals: process.env.NODE_ENV === 'development' ? {
       'vue': 'Vue',
       'vue-router': 'VueRouter',
       'element-ui': 'ELEMENT'
-    }
+    } : {},
+    plugins: [
+      // 开启gzip压缩
+      new CompressionWebpackPlugin(
+        {
+          filename: info => {
+            return `${info.path}.gz${info.query}`
+          },
+          algorithm: 'gzip',
+          threshold: 0, // 只有大小大于该值的资源会被处理 0
+          test: new RegExp('\\.(' + ['js'].join('|') + ')$'
+          ),
+          minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+          deleteOriginalAssets: false // 删除原文件
+        }
+      )
+    ]
   }
 }
