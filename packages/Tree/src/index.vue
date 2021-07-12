0349072062755be46db8ecb66c52b863bb5106e6
王亮 <1392830517@qq.com>
2021-07-12 11:32:44 +0800

    feat： 新增Tree组件

--- packages/Tree/src/index.vue
+++ packages/Tree/src/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <el-tree v-bind="$attrs" class="tree" v-on="$listeners"></el-tree>
+  <el-tree v-bind="$attrs" class="tree" v-on="$listeners">
+    <slot></slot>
+  </el-tree>
 </template>
 
 <script>
@@ -26,17 +28,19 @@ export default {
 
 <style lang="scss" scoped>
 ::v-deep .el-tree-node{
-  margin-bottom: 2px;
-  padding-left: 36px;
-  height: 28px;
-  display: table;
-  width: 100%;
-  box-sizing: border-box;
-  vertical-align: middle;
-  background: #FAFAFA;
-  border-radius: 4px;
-  &:hover {
-    background-color: #F1F8EF;
+  .el-tree-node__content {
+    padding-left: 36px;
+    margin-bottom: 2px;
+    display: table;
+    width: 100%;
+    box-sizing: border-box;
+    vertical-align: middle;
+    background: #FAFAFA;
+    border-radius: 4px;
+    height: 28px;
+    &:hover {
+      background-color: #F1F8EF;
+    }
   }
   .el-tree-node__expand-icon{
     font-size: 16px;
