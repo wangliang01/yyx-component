740f462d162cc5451d3e348279ecffd6dfb03e99
王亮 <1392830517@qq.com>
2021-08-05 09:52:53 +0800

    update: TablePro优化

--- packages/TablePro/src/index.vue
+++ packages/TablePro/src/index.vue
@@ -211,6 +211,7 @@ export default {
     /* 取消选择 */
     handleCancelSelection() {
       this.$refs.table.clearSelection()
+      this.$emit('clear-selection')
     },
     /* 选择框 */
     handleSelectChange(selection) {
