1a794e0305b5b5c0e7ae842089552627ef2a250b
王亮 <1392830517@qq.com>
2021-08-09 18:42:02 +0800

    update: 更新Select组件

---
+++ examples/views/audit-form/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="audit-form">
+    <y-audit-form v-model="form" :config="config" label-width="120px" label-suffix="："></y-audit-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {
+  },
+  props: {
+
+  },
+  data() {
+    return {
+      form: {},
+      config: {
+        auditResult: {
+          label: '审核结果',
+          prop: 'auditResult',
+          fieldType: 'Radio',
+          options: [
+            {
+              label: '通过',
+              value: '1',
+              cols: 12
+            },
+            {
+              label: '未通过',
+              value: '0',
+              cols: 12
+            }
+          ]
+        },
+        auditComponent: {
+          label: '审核结果',
+          prop: 'auditComponent',
+          fieldType: 'Input',
+          type: 'textarea'
+        }
+      }
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ packages/AuditForm/index.js
@@ -0,0 +1,7 @@
+import AuditForm from './src'
+
+AuditForm.install = Vue => {
+  Vue.component(AuditForm.name, AuditForm)
+}
+
+export default AuditForm
