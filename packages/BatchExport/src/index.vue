448ab33049143395076a677910efa8d511ae9578
王亮 <1392830517@qq.com>
2021-10-14 22:56:30 +0800

    update: 新增excel导出功能

--- packages/BatchExport/src/index.vue
+++ packages/BatchExport/src/index.vue
@@ -30,15 +30,6 @@ export default {
       type: String,
       default: '采购单详情'
     }
-
-  },
-  data() {
-    return {
-      excelData: []
-    }
-  },
-  mounted() {
-
   },
   methods: {
     //  导出excel
@@ -47,18 +38,23 @@ export default {
         // 当导出数据为空时，提示用户
         return this.$message.warning('数据为空， 无法进行导出')
       }
+      let tableData = []
       if (this.format && typeof this.format === 'function') {
-        this.excelData = this.format(this.data)
+        tableData = this.format(this.data)
       } else {
-        this.excelData = this.data
+        tableData = this.data
       }
+
       // excel表头
       const excelHeader = this.buildHeader(this.columns)
       const headerRows = excelHeader.length
+      // excel数据
+      const excelData = this.extractData(tableData, this.columns)
+      // 表头 + 数据
+      const sheetData = [...excelHeader, ...excelData]
       // 生成worksheet
-      const worksheet = XLSX.utils.aoa_to_sheet(excelHeader, headerRows)
-      console.log(worksheet)
-      debugger
+      const worksheet = XLSX.utils.aoa_to_sheet(sheetData, headerRows)
+
       // 冻结表头
       worksheet['!freeze'] = {
         xSplit: '1',
@@ -88,11 +84,87 @@ export default {
 
       const wbout = XLSX.write(workbook, writingOptions)
 
-      console.log('--------')
-      console.log(wbout)
       const blobParts = this.stringToBuffer(wbout)
       const blob = new Blob([blobParts], { type: 'application/octet-stream' })
       this.downloadExcel(blob, this.sheetName + '.xlsx')
+    },
+    // 从github复制过来
+    aoa_to_sheet(data, headerRows) {
+      const ws = {}
+      const range = { s: { c: 10000000, r: 10000000 }, e: { c: 0, r: 0 }}
+      for (let R = 0; R !== data.length; ++R) {
+        for (let C = 0; C !== data[R].length; ++C) {
+          if (range.s.r > R) {
+            range.s.r = R
+          }
+          if (range.s.c > C) {
+            range.s.c = C
+          }
+          if (range.e.r < R) {
+            range.e.r = R
+          }
+          if (range.e.c < C) {
+            range.e.c = C
+          }
+          // / 这里生成cell的时候，使用上面定义的默认样式
+          const cell = {
+            v: data[R][C] || '',
+            s: {
+              font: { name: '宋体', sz: 11, color: { auto: 1 }},
+              alignment: {
+                // / 自动换行
+                wrapText: 1,
+                // 居中
+                horizontal: 'center',
+                vertical: 'center',
+                indent: 0
+              }
+            }
+          }
+          // 头部列表加边框
+          if (R < headerRows) {
+            cell.s.border = {
+              top: { style: 'thin', color: { rgb: '000000' }},
+              left: { style: 'thin', color: { rgb: '000000' }},
+              bottom: { style: 'thin', color: { rgb: '000000' }},
+              right: { style: 'thin', color: { rgb: '000000' }}
+            }
+            cell.s.fill = {
+              patternType: 'solid',
+              fgColor: { theme: 3, 'tint': 0.3999755851924192, rgb: 'DDD9C4' },
+              bgColor: { theme: 7, 'tint': 0.3999755851924192, rgb: 'E7EBF4' }
+            }
+          }
+          const cell_ref = XLSX.utils.encode_cell({ c: C, r: R })
+          if (typeof cell.v === 'number') {
+            cell.t = 'n'
+          } else if (typeof cell.v === 'boolean') {
+            cell.t = 'b'
+          } else {
+            cell.t = 's'
+          }
+          ws[cell_ref] = cell
+        }
+      }
+      if (range.s.c < 10000000) {
+        ws['!ref'] = XLSX.utils.encode_range(range)
+      }
+      return ws
+    },
+    extractData(tableData, columns) {
+      // 导出的列结果集
+      const excelRows = []
+      for (const item of tableData) {
+        excelRows.push(this.getRow(item, columns))
+      }
+      return excelRows
+    },
+    getRow(item, columns) {
+      const row = []
+      columns.forEach(column => {
+        row.push(item[column.prop])
+      })
+      return row
     },
     downloadExcel(blob, downloadName) {
       let url
@@ -122,7 +194,9 @@ export default {
     },
     // 构建表头
     buildHeader(columns) {
-      return columns.map(column => [column.label])
+      const header = []
+      header.push(columns.map(column => column.label))
+      return header
     }
   }
 }
