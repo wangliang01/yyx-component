332470ff527a1fef81d512ce6bd5ae83908e448d
Huanghai <huanghai2@newhope.cn>
2021-12-02 10:24:07 +0800

    完善虚拟表格，滚动到底部时，滚动条会弹跳下，完善input滚动时不能失去焦点的问题...

--- packages/Table/src/VirtualList.vue
+++ packages/Table/src/VirtualList.vue
@@ -6,6 +6,7 @@
     :row-style="rowStyle"
     :height="height"
     :size="size"
+    :changed-data="null"
     row-key="id"
     v-on="$listeners"
   >
@@ -73,26 +74,27 @@ export default {
     }
   },
   computed: {
-    maxRows() {
-      return Math.ceil((this.height - this.headerHeight) / this.rowHeight) + 4
-    },
     computedColumns() {
       return this.columns.map(col => {
         col.showOverflowTooltip = true
         return col
       })
     },
-    virtualData: {
-      get() {
-        return this.originData.slice(this.start, this.start + this.maxRows)
-      }
+    virtualData() {
+      return this.originData.slice(this.start, this.start + this.maxRows)
+    },
+    maxRows() { // 最大行数
+      return Math.ceil((this.height - this.headerHeight) / this.rowHeight) + 4
+    },
+    firstMaxHeight() { // 第一行最大高度
+      return this.tableVirtualHeight - (this.maxRows * this.rowHeight)
     },
     firstRowHeight() { // 第一行高度
       const firstRowHeight = this.tableScrollTop - this.rowHeight - (this.tableScrollTop % this.rowHeight)
-      return firstRowHeight < 0 ? 0 : firstRowHeight
+      return firstRowHeight >= this.firstMaxHeight ? this.firstMaxHeight : firstRowHeight
     },
     lastRowHeight() { // 最后一行高度
-      return this.tableVirtualHeight - this.firstRowHeight - (this.maxRows * this.rowHeight)
+      return this.firstMaxHeight - this.firstRowHeight
     }
   },
   watch: {
@@ -114,16 +116,23 @@ export default {
     const ElTable = this.$refs.ElTable
     this.bodyWrapper = ElTable.bodyWrapper
     this.bodyWrapper.onscroll = this.onVirtualScroll
+    // 新增一个隐藏 input 解决input 滚动时 失去焦点的问题
+    this.input = document.createElement('input')
+    this.input.style.position = 'fixed'
+    this.input.style.width = '10px'
+    this.input.style.left = '-100px'
+    document.body.appendChild(this.input)
   },
-  activated() {
-    // 在 keep-alive缓存组件里，第二次进来时，滚动条默认会到顶部，此时缓存的数据 有问题
+  activated() { // 在 keep-alive缓存组件里，第二次进来时，滚动条默认会到顶部，此时缓存的数据 有问题
     setTimeout(() => {
       this.bodyWrapper.scrollTop = this.tableScrollTop
     })
   },
-  beforeDestroy() {
+  beforeDestroy() { // 销毁时，释放内存
+    document.body.removeChild(this.input)
     this.bodyWrapper.onscroll = null
     this.bodyWrapper = null
+    this.input = null
   },
   methods: {
     // 表格行样式
@@ -155,7 +164,10 @@ export default {
       this.tableScrollTop = scrollTop
       let start = Math.floor(this.tableScrollTop / this.rowHeight) - 2
       start = start < 0 ? 0 : start
+      const maxStart = this.originData.length - this.maxRows
+      if (start >= maxStart) start = maxStart
       this.start = start
+      this.input.focus()
     }
   }
 }
