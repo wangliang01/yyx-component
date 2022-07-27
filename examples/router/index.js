3d64f1c6fe0fef9e435def4030fa38fb913c66e8
王亮 <1392830517@qq.com>
2022-07-27 11:50:17 +0800

    feat: 新增DatePicker组件

--- examples/router/index.js
+++ examples/router/index.js
@@ -232,6 +232,12 @@ const routes = [
         name: 'Copy',
         component: () => import('../views/copy'),
         meta: { title: '复制' }
+      },
+      {
+        path: '/date-picker',
+        name: 'DatePicker',
+        component: () => import('../views/date-picker'),
+        meta: { title: 'datePicker' }
       }
     ]
   }
--- packages/index.js
+++ packages/index.js
@@ -54,12 +54,13 @@ import AddressPro from './AddressPro'
 import Icon from './Icon'
 import Title from './Title'
 import Collapse from './Collapse'
+import DatePicker from './DatePicker'
 
 const { YCategoryCascader, YAddressCascader, YCascader } = Cascader
 const { YUpload, YAvatar } = Upload
 
 // 组件列表
-const components = [YButton, YTable, YForm, YInputNumber, YBackTop, YUpload, YUploadList, YTablePro, YCategoryCascader, YAddressCascader, YBatchImport, YSkuImport, YInput, YTreeOrg, YCascader, YPrint, YAvatar, Tag, Dialog, Drawer, ImageViewer, Tips, Image, ImageList, TableDialog, Text, Card, Block, Tree, CardForm, CascaderPanel, Container, Checkbox, CheckboxGroup, TableMate, Transfer, AuditForm, Select, TableAir, Calendar, BatchExport, VirtualList, DataBoard, ButtonMore, Descriptions, SelectPro, Copy, AddressPro, Icon, Title, Collapse]
+const components = [YButton, YTable, YForm, YInputNumber, YBackTop, YUpload, YUploadList, YTablePro, YCategoryCascader, YAddressCascader, YBatchImport, YSkuImport, YInput, YTreeOrg, YCascader, YPrint, YAvatar, Tag, Dialog, Drawer, ImageViewer, Tips, Image, ImageList, TableDialog, Text, Card, Block, Tree, CardForm, CascaderPanel, Container, Checkbox, CheckboxGroup, TableMate, Transfer, AuditForm, Select, TableAir, Calendar, BatchExport, VirtualList, DataBoard, ButtonMore, Descriptions, SelectPro, Copy, AddressPro, Icon, Title, Collapse, DatePicker]
 
 // 定义install方法，接收Vue作为参数，通过Vue.use()的方式注册组件
 const install = Vue => {
@@ -132,5 +133,6 @@ export default {
   AddressPro,
   Icon,
   Title,
-  Collapse
+  Collapse,
+  DatePicker
 }
